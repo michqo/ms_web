41acbf2d26494fd138d2a6a6848cff65bbda4414
michqo <42279279+michqo@users.noreply.github.com>
2024-12-28 13:25:02 +0100

    feat: links dropdown

---
+++ src/lib/components/ui/nav/links.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+	import { buttonVariants } from '@/components/ui/button';
+	import * as DropdownMenu from '@/components/ui/dropdown-menu';
+
+  const routesMap: Record<string, string> = {
+    '/': 'Home',
+  }
+
+  const currentPage = $derived($page.url.pathname ? routesMap[$page.url.pathname] : 'Home');
+</script>
+
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger class={buttonVariants({ variant: 'ghost' })}>
+    {currentPage}
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content>
+    <DropdownMenu.Group>
+      {#each Object.entries(routesMap) as [route, name]}
+        <DropdownMenu.Item>
+          <a href={route}>{name}</a>
+        </DropdownMenu.Item>
+      {/each}
+    </DropdownMenu.Group>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
--- src/lib/components/ui/nav/nav.svelte
+++ src/lib/components/ui/nav/nav.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import { Button } from '@/components/ui/button';
 	import { Github } from 'lucide-svelte';
 	import ThemeToggle from './themetoggle.svelte';
 	import Actions from './actions.svelte';
+	import Links from './links.svelte';
 
 	let { user }: { user?: string } = $props();
-
-	let pathname = $derived($page.url.pathname.length > 1 ? $page.url.pathname.split('/')[1] : '/');
 </script>
 
 <div
 	class="sticky inset-0 z-50 flex w-full justify-center border-b border-border/40 bg-primary-foreground/60 p-4 backdrop-blur-md"
 >
 	<div class="flex w-full max-w-xl items-center justify-between">
-		<h1 class="text-2xl font-bold">
-			{pathname}
-		</h1>
+		<Links />
 		<div class="flex items-center gap-x-2">
 			<ThemeToggle />
 			<Button variant="ghost" size="icon" href="https://github.com/michqo/ms_web" target="_blank"
