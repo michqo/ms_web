242fc0e9e412ca5c29a71220487d5b552c6bb41b
michqo <42279279+michqo@users.noreply.github.com>
2024-12-29 09:45:43 +0100

    feat: enhance links dropdown

--- src/lib/components/ui/nav/links.svelte
+++ src/lib/components/ui/nav/links.svelte
@@ -2,23 +2,43 @@
   import { page } from '$app/stores';
 	import { buttonVariants } from '@/components/ui/button';
 	import * as DropdownMenu from '@/components/ui/dropdown-menu';
+	import { Home } from 'lucide-svelte';
 
-  const routesMap: Record<string, string> = {
-    '/': 'Home',
+  type Route = {
+    name: string;
+    icon: any;
+  }
+  
+  const routesMap: Record<string, Route> = {
+    '/': {
+      name: 'Home',
+      icon: Home,
+    },
   }
 
-  const currentPage = $derived($page.url.pathname ? routesMap[$page.url.pathname] : 'Home');
+  const homeLink = {
+    name: 'Home',
+    icon: Home,
+  }
+
+  const currentPage = $derived($page.url.pathname ? routesMap[$page.url.pathname] : homeLink);
 </script>
+
+{#snippet link(route: string, data: Route)}
+  <data.icon />
+  <a href={route}>{data.name}</a>
+{/snippet}
 
 <DropdownMenu.Root>
   <DropdownMenu.Trigger class={buttonVariants({ variant: 'ghost' })}>
-    {currentPage}
+    <currentPage.icon />
+    <span>{currentPage.name}</span>
   </DropdownMenu.Trigger>
   <DropdownMenu.Content>
     <DropdownMenu.Group>
-      {#each Object.entries(routesMap) as [route, name]}
+      {#each Object.entries(routesMap) as [route, data]}
         <DropdownMenu.Item>
-          <a href={route}>{name}</a>
+          {@render link(route, data)}
         </DropdownMenu.Item>
       {/each}
     </DropdownMenu.Group>
