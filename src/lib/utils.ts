2bca73caf7a182960eabd736a04a56d345ac5d49
michqo <42279279+michqo@users.noreply.github.com>
2025-07-17 11:18:28 +0200

    feat: upgrade shadcn theme colors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { type ClassValue, clsx } from 'clsx';
+import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
@@ -10,6 +10,4 @@ export type WithoutChild<T> = T extends { child?: any } ? Omit<T, 'child'> : T;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, 'children'> : T;
 export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
-export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & {
-	ref?: U | null;
-};
+export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
